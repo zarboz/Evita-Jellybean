ca90ed5bec118e1fd859f7074b8b865e7ee87b98
Bryan Smith <zarboz@gmail.com>
2013-02-19 08:46:18 -0800

    removing faux's name from kmsg leaving it everywhere else just dont want to see his name on my kmsg really

--- arch/arm/mach-msm/acpuclock-8960.c
+++ arch/arm/mach-msm/acpuclock-8960.c
@@ -1619,7 +1619,7 @@ void acpuclk_set_vdd(unsigned int khz, int vdd_uv) {
 
 		acpu_freq_tbl[i].vdd_core = new_vdd_uv;
 	}
-	pr_warn("faux123: user voltage table modified!\n");
+	pr_warn("user voltage table modified!\n");
 	mutex_unlock(&driver_lock);
 }
 #endif	/* CONFIG_CPU_VOTALGE_TABLE */
